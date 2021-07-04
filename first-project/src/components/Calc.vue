200f5e2eddebe455c5c10c4077accb9435a20662
Ruslan <Mediumgr@yandex.ru>
2021-07-05 02:59:17 +0300

    ready

--- first-project/src/components/Calc.vue
+++ first-project/src/components/Calc.vue
@@ -4,7 +4,10 @@
         <input v-model.number.trim="operand1"/>
         <input v-model.number.trim="operand2"/>
         = {{result}}
-      </div>
+        </div>
+        <div class="error">
+          {{error}}
+        </div>
       <div class="keyboard">
         <button @click="Sum">+</button>
         <button @click="Subtraction">-</button>
@@ -15,12 +18,12 @@
       </div>
         <input type="checkbox" id="checkbox" v-model="checked">
         <label for="checkbox">{{status}}</label>
-      <div>
+      <div class="button">
           <button v-for="btn in buttons" :key='btn' @click="changeInput(btn)" v-show="checked">{{btn}}</button>
       </div>
-        <input type="radio" id="one" value="Операнд1" v-model="picked">
+        <input type="radio" id="one" value="operand1" v-model="picked">
         <label for="one">Операнд1</label>
-        <input type="radio" id="two" value="Операнд2" v-model="picked">
+        <input type="radio" id="two" value="operand2" v-model="picked">
         <label for="two">Операнд2</label>
     </div>
 </template>
@@ -33,40 +36,67 @@ export default {
       operand1: '',
       operand2: '',
       result: '',
+      error: '',
       checked: false,
       status: 'Отобразить клавиатуру',
       buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 'убрать один символ'],
-      picked: 'Операнд1'
+      picked: ''
     }
   },
   methods: {
     Sum () {
+      this.error = ''
       this.result = this.operand1 + this.operand2
     },
     Subtraction () {
+      this.error = ''
       this.result = this.operand1 - this.operand2
     },
     Divide () {
       if (this.operand2 !== 0) {
+        this.error = ''
         this.result = this.operand1 / this.operand2
-      } else this.result = 'Ошибка: На ноль делить нельзя'
+      } else {
+        this.error = 'Ошибка: На ноль делить нельзя'
+        this.result = 'Error'
+      }
     },
     Multiply () {
-      this.result = this.operand1 * this.operand2
+      if (this.operand2 !== 0) {
+        this.error = ''
+        this.result = this.operand1 * this.operand2
+      } else {
+        this.error = 'Ошибка: На ноль умножать нельзя'
+        this.result = 'Error'
+      }
     },
     Exponentiation () {
+      this.error = ''
       this.result = this.operand1 ** this.operand2
     },
     IntegerDivision () {
       if (this.operand2 !== 0) {
+        this.error = ''
         this.result = Math.floor(this.operand1 / this.operand2)
-      } else this.result = 'Ошибка: На ноль делить нельзя'
+      } else {
+        this.error = 'Ошибка: На ноль делить нельзя'
+        this.result = 'Error'
+      }
     },
     changeInput (number) {
-      if (number !== this.buttons[9]) {
+      if (number !== this.buttons[9] && this.picked === 'operand1') {
         this.operand1 = parseInt(`${this.operand1}` + number)
-      } if (number === this.buttons[9]) {
-        console.log(this.operand1.slice(lastIndexOf(this.operand1)))
+      } else if (number === this.buttons[9] && this.picked === 'operand1') {
+        const array = this.operand1.toString().split('').map(int => parseInt(int))
+        array.pop()
+        this.operand1 = parseInt(array.join(''))
+      }
+      if (number !== this.buttons[9] && this.picked === 'operand2') {
+        this.operand2 = parseInt(`${this.operand2}` + number)
+      } else if (number === this.buttons[9] && this.picked === 'operand2') {
+        const array = this.operand2.toString().split('').map(int => parseInt(int))
+        array.pop()
+        this.operand2 = parseInt(array.join(''))
       }
     }
   }
@@ -75,5 +105,18 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.div {
+  margin:250px 100px;
+  align-items: center;
+  text-align: center;
+}
+.error {
+  color:red;
+}
+.keyboard {
+  padding: 10px 0;
+}
+.button {
+  padding: 15px 0;
+}
 </style>
